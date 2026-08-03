b76dc78311010314fa53aef6cb61e6fbbd4867c5
raf0670 <swagbondhu@gmail.com>
2026-08-03 09:06:00 +0600

    test connection function added

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -3,4 +3,13 @@ const pgp = require('pg-promise')();
 const connectionString = process.env.DATABASE_URL;
 const db = pgp(connectionString);
 
-module.exports = { db, pgp };
+const testConnection = async () => {
+    try {
+        const res = await db.one('SELECT NOW() AS now');
+        console.log(`Database Connected ${res.now}`);
+    } catch (error) {
+        console.error('Database connection failed:', error);
+    }
+};
+
+module.exports = { db, pgp, testConnection };
